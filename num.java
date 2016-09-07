60c0f1872f09a6c450504d78ba1c4c5a4649f1c7
Keerthikaarivalagan <keerthikaarivalagan05@gmail.com>
2016-09-07 10:36:23 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class num
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>0)
+{
+System.out.println("positive");
+}
+else
+System.out.println("negative");
+}
